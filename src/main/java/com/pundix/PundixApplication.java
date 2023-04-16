325b83c29b94ff93f4637a30e9ee34a39c792081
kerim.yucel <kerim.yucel@trendyol.com>
2023-04-16 20:29:48 +0300

    change some pattern

--- src/main/java/com/pundix/PundixApplication.java
+++ src/main/java/com/pundix/PundixApplication.java
@@ -2,11 +2,7 @@ package com.pundix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.pundix")
-@EntityScan("com.pundix")
 @SpringBootApplication
 public class PundixApplication {
 
